5b38a9195809b1c6790d689f3b82402acdcee7c9
laysoniesp <laysoniesp@gmail.com>
2026-09-04 15:32:49 -0300

    👍

--- RANDOM_QUERY/R_Query1.sql
+++ RANDOM_QUERY/R_Query1.sql
@@ -1,2 +1,11 @@
 USE [bdpedido];
 GO
+
+select * from Categorias
+
+select * from Produtos
+where CodigoDaCategoria=3
+
+select * from Produtos p 
+join Fornecedores f
+on p.CodigoDoFornecedor = f.CodigoDoFornecedor
