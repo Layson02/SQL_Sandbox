768b3782e06f027245d0ed43531a3ee0e5c7fe4f
laysoniesp <laysoniesp@gmail.com>
2026-09-04 15:48:40 -0300

    adicionar consulta SQL ausente para detalhes de produtos e fornecedores

--- RANDOM_QUERY/R_Query1.sql
+++ RANDOM_QUERY/R_Query1.sql
@@ -9,3 +9,10 @@ where CodigoDaCategoria=3
 select * from Produtos p 
 join Fornecedores f
 on p.CodigoDoFornecedor = f.CodigoDoFornecedor
+
+select p.DataDoPedido, p.CodigoDoFuncionario,f.Nome,MONTH(p.DataDoPedido),YEAR(p.DataDoPedido)
+from Pedidos p
+join Funcionarios f
+on p.CodigoDoFuncionario = f.CodigoDoFuncionario
+WHERE YEAR(p.DataDoPedido) = 1996 AND MONTH(p.DataDoPedido) = 8
+ORDER BY p.CodigoDoFuncionario
